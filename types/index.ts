7eb2119536f7e2fd314a9200c9a7d1af63d0ecf5
Shajid <shajidsr12@gmail.com>
2026-07-05 01:01:53 +0600

    define intitial types

---
+++ types/index.ts
@@ -0,0 +1,36 @@
+export type Priority  = 'High' | 'Medium' | 'Low'
+export type ColumnId = 'Backlog' | 'Todo' | 'In Progress' | 'Review'
+
+export interface Label {
+    id:string,
+    name:string,
+    color:string,
+}
+
+export interface Assignee{
+    id:string,
+    name:string,
+    avatarUrl?:string // placeholder fornow 
+
+
+}
+
+export interface Task {
+    id:string,
+    title:string,
+    description:string,
+    status:ColumnId,
+    assignee?:Assignee,
+    lables:Label[],
+    priority: Priority,
+    dueDate?:string ;
+
+}
+
+export interface BoardData{
+    tasks:Task[],
+    columns:{
+        id:ColumnId,
+        title:string; 
+    }[]
+}
